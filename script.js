ab61bdb806679072737211c795acc2403dedbed9
Chaitanya <104759918+Chai5K5@users.noreply.github.com>
2026-04-06 19:58:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// rock
+const rock = document.querySelector(".rock")
+// paper
+const paper = document.querySelector(".paper")
+// scissors
+const sciss = document.querySelector(".sciss")
+
+// array of r, p, s
+// score
+//pick
